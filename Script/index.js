c5ec150787bb44d147280cb5fb5a5c51a540edfe
Mazharul-2003 <smmir201@gmail.com>
2026-02-28 14:37:30 +0600

    final Commit Alhamdulillah

--- Script/index.js
+++ Script/index.js
@@ -38,25 +38,25 @@ function toogleTab(id) {
 
   if (selectedTab === allTab) {
     allContainer.classList.remove('hidden');
-    if(allContainer.children.length == 0) {
+    if (allContainer.children.length == 0) {
       emptyState.classList.remove("hidden");
     }
   }
   else if (selectedTab === appliedTab) {
     appliedContainer.classList.remove('hidden');
-    if(appliedContainer.children.length == 0) {
+    if (appliedContainer.children.length == 0) {
       emptyState.classList.remove("hidden");
     }
   }
   else if (selectedTab === interviewTab) {
     interviewContainer.classList.remove('hidden');
-    if(interviewContainer.children.length == 0) {
+    if (interviewContainer.children.length == 0) {
       emptyState.classList.remove("hidden");
     }
   }
   else {
     rejectedContainer.classList.remove('hidden');
-    if(rejectedContainer.children.length == 0) {
+    if (rejectedContainer.children.length == 0) {
       emptyState.classList.remove("hidden");
     }
   }
@@ -74,55 +74,69 @@ toogleTab(currentTab);
 
 // Event Delegation on All Card (Card Container)
 document.getElementById("job-container")
-  .addEventListener("click", function(event) {
+  .addEventListener("click", function (event) {
     const clickedElement = event.target;
     const card = clickedElement.closest(".card");
     const cardParent = card.parentNode;
     const innerCardStat = document.querySelector(".card-stats");
 
     if (clickedElement.classList.contains('applied')) {
-      innerCardStat.innerText = "Applied";
-      appliedContainer.appendChild(card);
+      card.querySelector(".card-stats").innerText = "Applied";
+
+      if (card.parentNode.id !== "applied-container") {
+        const clone = card.cloneNode(true);
+        appliedContainer.appendChild(clone);
+      }
     }
+
     if (clickedElement.classList.contains('interview')) {
-      innerCardStat.innerText = "Interviewed";
-      interviewContainer.appendChild(card);
+      card.querySelector(".card-stats").innerText = "Interviewed";
+
+      if (card.parentNode.id !== "interview-container") {
+        const clone = card.cloneNode(true);
+        interviewContainer.appendChild(clone);
+      }
     }
+
     if (clickedElement.classList.contains('rejected')) {
-      innerCardStat.innerText = "rejected";
-      rejectedContainer.appendChild(card);
+      card.querySelector(".card-stats").innerText = "Rejected";
+
+      if (card.parentNode.id !== "rejected-container") {
+        const clone = card.cloneNode(true);
+        rejectedContainer.appendChild(clone);
+      }
     }
     if (clickedElement.classList.contains('delete-btn')) {
-      cardParent.removeChild(card); 
+      cardParent.removeChild(card);
     }
     updateStat();
   });
 
-  function updateStat() {
-    // totalStat.innerText = allContainer.children.length;
-    // appliedStat.innerText = appliedContainer.children.length;
-    // interviewStat.innerText = interviewContainer.children.length;
-    // rejectedStat.innerText = rejectedContainer.children.length;
+function updateStat() {
+  // totalStat.innerText = allContainer.children.length;
+  // appliedStat.innerText = appliedContainer.children.length;
+  // interviewStat.innerText = interviewContainer.children.length;
+  // rejectedStat.innerText = rejectedContainer.children.length;
 
-    const counts = {
-      "all-tab": allContainer.children.length,
-      "applied-tab": appliedContainer.children.length,
-      "interview-tab": interviewContainer.children.length,
-      "rejected-tab": rejectedContainer.children.length
-    }
-    totalStat.innerText = counts["all-tab"];
-    appliedStat.innerText = counts["applied-tab"];
-    interviewStat.innerText = counts["interview-tab"];
-    rejectedStat.innerText = counts["rejected-tab"];
-
-    availableStat.innerText = counts[currentTab];
-
-    if(counts[currentTab] == 0) {
-      emptyState.classList.remove("hidden");
-    }
-    else {
-      emptyState.classList.add("hidden");
-    }
+  const counts = {
+    "all-tab": allContainer.children.length,
+    "applied-tab": appliedContainer.children.length,
+    "interview-tab": interviewContainer.children.length,
+    "rejected-tab": rejectedContainer.children.length
   }
+  totalStat.innerText = counts["all-tab"];
+  appliedStat.innerText = counts["applied-tab"];
+  interviewStat.innerText = counts["interview-tab"];
+  rejectedStat.innerText = counts["rejected-tab"];
 
-  updateStat();
+  availableStat.innerText = counts[currentTab];
+
+  if (counts[currentTab] == 0) {
+    emptyState.classList.remove("hidden");
+  }
+  else {
+    emptyState.classList.add("hidden");
+  }
+}
+
+updateStat();
